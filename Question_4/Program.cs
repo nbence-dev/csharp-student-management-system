9d6bff26aba1e51192288d3cf558e08309f69e3f
Nicholas <nicholasbence12@gmail.com>
2025-05-27 20:39:52 +0200

    Version 1. LinkedLists

---
+++ Question_4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Question_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
